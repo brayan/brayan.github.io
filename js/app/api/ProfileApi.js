61a57bb3b2f2b3677d758cb0ebdbda3fd14beda7
Brayan Bedritchuk <brayan.bedritchuk@gmail.com>
2022-11-08 09:26:29 -0300

    Update profile base url

--- js/app/api/ProfileApi.js
+++ js/app/api/ProfileApi.js
@@ -1,9 +1,9 @@
-const BASE_API = "https://brayan.com.br/api/";
+const BASE_API = "https://brayan.com.br/api";
 
 export default class ProfileApi {
     
     async fetchProfile() {
-        return fetch(`${BASE_API}profile.json`).then(response => response.json());
+        return fetch(`${BASE_API}/profile`).then(response => response.json());
     }
 
 }
